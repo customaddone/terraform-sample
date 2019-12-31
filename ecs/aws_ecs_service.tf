5bee4c563b01a8425a20fcabd97f4a5d3171bf5f
customfolk <custom1485@gmail.com>
2019-12-31 21:34:40 +0900

    ec2?

--- ecs/aws_ecs_service.tf
+++ ecs/aws_ecs_service.tf
@@ -6,14 +6,6 @@ resource "aws_ecs_service" "example" {
   launch_type                       = "EC2"
 }
 
-module "nginx_sg" {
-  source      = "./aws_security_group"
-  name        = "nginx-sg"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.example_id
-  port        = 80
-  cidr_blocks = ["10.0.0.0/16"]
-}
-
 output "ecs_service_name" {
   value = aws_ecs_service.example.name
 }
--- iam/aws_iam_policy.tf
+++ iam/aws_iam_policy.tf
@@ -2,26 +2,8 @@ data "aws_iam_policy_document" "ec2" {
   statement {
     effect    = "Allow"
     resources = ["*"]
-
     actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:GetRepositoryPolicy",
-      "ecr:DescribeRepositories",
-      "ecr:ListImages",
-      "ecr:DescribeImages",
-      "ecr:BatchGetImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:PutImage",
+      "sts:AssumeRole"
     ]
   }
 }
